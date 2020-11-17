1a0822a46a54d9fce4e8335292cf43f0289399f7
Nicolas TROUIN <nicolas.trouin@insa-lyon.fr>
2020-11-17 10:28:56 +0100

    Auto reformat

--- src/stream/ClientThread.java
+++ src/stream/ClientThread.java
@@ -11,33 +11,33 @@ import java.io.*;
 import java.net.*;
 
 public class ClientThread
-	extends Thread {
-	
-	private Socket clientSocket;
-	
-	ClientThread(Socket s) {
-		this.clientSocket = s;
-	}
+        extends Thread {
 
- 	/**
-  	* receives a request from client then sends an echo to the client
-  	* @param clientSocket the client socket
-  	**/
-	public void run() {
-    	  try {
-    		BufferedReader socIn = null;
-    		socIn = new BufferedReader(
-    			new InputStreamReader(clientSocket.getInputStream()));    
-    		PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
-    		while (true) {
-    		  String line = socIn.readLine();
-    		  socOut.println(line);
-    		}
-    	} catch (Exception e) {
-        	System.err.println("Error in EchoServer:" + e); 
+    private Socket clientSocket;
+
+    ClientThread(Socket s) {
+        this.clientSocket = s;
+    }
+
+    /**
+     * receives a request from client then sends an echo to the client
+     *
+	 **/
+    public void run() {
+        try {
+            BufferedReader socIn = null;
+            socIn = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
+            while (true) {
+                String line = socIn.readLine();
+                socOut.println(line);
+            }
+        } catch (Exception e) {
+            System.err.println("Error in EchoServer:" + e);
         }
-       }
-  
-  }
+    }
+
+}
 
   
--- src/stream/EchoServerMultiThreaded.java
+++ src/stream/EchoServerMultiThreaded.java
@@ -10,33 +10,33 @@ package stream;
 import java.io.*;
 import java.net.*;
 
-public class EchoServerMultiThreaded  {
-  
- 	/**
-  	* main method
-	* @param EchoServer port
-  	* 
-  	**/
-       public static void main(String args[]){ 
+public class EchoServerMultiThreaded {
+
+    /**
+     * main method
+     *
+     * @param args port
+     **/
+    public static void main(String args[]) {
         ServerSocket listenSocket;
-        
-  	if (args.length != 1) {
-          System.out.println("Usage: java EchoServer <EchoServer port>");
-          System.exit(1);
-  	}
-	try {
-		listenSocket = new ServerSocket(Integer.parseInt(args[0])); //port
-		System.out.println("Server ready..."); 
-		while (true) {
-			Socket clientSocket = listenSocket.accept();
-			System.out.println("Connexion from:" + clientSocket.getInetAddress());
-			ClientThread ct = new ClientThread(clientSocket);
-			ct.start();
-		}
+
+        if (args.length != 1) {
+            System.out.println("Usage: java EchoServer <EchoServer port>");
+            System.exit(1);
+        }
+        try {
+            listenSocket = new ServerSocket(Integer.parseInt(args[0])); //port
+            System.out.println("Server ready...");
+            while (true) {
+                Socket clientSocket = listenSocket.accept();
+                System.out.println("Connexion from:" + clientSocket.getInetAddress());
+                ClientThread ct = new ClientThread(clientSocket);
+                ct.start();
+            }
         } catch (Exception e) {
             System.err.println("Error in EchoServer:" + e);
         }
-      }
-  }
+    }
+}
 
   
